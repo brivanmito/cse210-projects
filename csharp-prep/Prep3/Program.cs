c5148f408b03700b01f019e59d07c759697704c3
Bryan <bryanmt496@gmail.com>
2023-04-24 17:39:34 -0500

    Prep 3, making a game using do-while

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        // Part 1 and 2, in the line 9 the user specified the number.
+        // int magicNumber = int.Parse(Console.ReadLine());
+        int guess;
+
+        string continueGame = "yes";
+        
+        while(continueGame == "yes")
+        {
+            // Part 3. We generate a random number each time the 
+            // user press yes.
+            int magicNumber = randomGenerator.Next(1, 101);
+            do
+            {
+                // Asking a guess
+                Console.Write("What is your guess?: ");
+                guess = int.Parse(Console.ReadLine());
+                // Check if the user guess the Magic Number
+                if(guess > magicNumber)
+                {
+                    Console.WriteLine("Higher.");
+                }
+                else if(guess < magicNumber)
+                {
+                    Console.WriteLine("Lower.");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!.");
+                    Console.WriteLine("Game over.");
+                }
+            } while(guess != magicNumber);
+            Console.Write("Do you want to play again?:\n Type 'yes' or 'no': ");
+            continueGame = Console.ReadLine().ToLower();
+        }
+
     }
 }
