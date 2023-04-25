8167155e13267f3d1c8f5f4bd75527b09d18f88b
Bryan <bryanmt496@gmail.com>
2023-04-25 17:21:51 -0500

    Addind comments on the code

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main(string[] args)
     {
         // Calling functions
-        DisplayName();
+        DisplayWelcome();
         string name = PromptUserName();
         int favoriteNumber = PromptUserNumber();
         int numberSquare = SquareNumber(favoriteNumber);
@@ -14,28 +14,32 @@ class Program
 
 
 
-
-        static void DisplayName()
+        // Functions
+        static void DisplayWelcome()
         {
             Console.WriteLine("Welcome to the Program!");
         }
+
         static string PromptUserName()
         {
             Console.Write("Please enter your name: ");
             string Name = Console.ReadLine();
             return Name;
         }
+
         static int PromptUserNumber()
         {
             Console.Write("Please enter your favorite number: ");
             int favoriteNumber = int.Parse(Console.ReadLine());
             return favoriteNumber;
         }
+
         static int SquareNumber(int favoriteNumber)
         {
             int numberSquared = favoriteNumber * favoriteNumber;
             return numberSquared;
         }
+        
         static void DisplayResult(string name, int numberSquared)
         {
             Console.WriteLine($"{name}, the square of your number is {numberSquared}");
