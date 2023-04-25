b421d6a1c97bcb90c509c5227939d0538be8b2e9
Bryan <bryanmt496@gmail.com>
2023-04-25 17:20:01 -0500

    Prep 5 working with Functions

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        // Calling functions
+        DisplayName();
+        string name = PromptUserName();
+        int favoriteNumber = PromptUserNumber();
+        int numberSquare = SquareNumber(favoriteNumber);
+        DisplayResult(name, numberSquare);
+
+
+
+
+
+        static void DisplayName()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            string Name = Console.ReadLine();
+            return Name;
+        }
+        static int PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            int favoriteNumber = int.Parse(Console.ReadLine());
+            return favoriteNumber;
+        }
+        static int SquareNumber(int favoriteNumber)
+        {
+            int numberSquared = favoriteNumber * favoriteNumber;
+            return numberSquared;
+        }
+        static void DisplayResult(string name, int numberSquared)
+        {
+            Console.WriteLine($"{name}, the square of your number is {numberSquared}");
+        }
     }
 }
