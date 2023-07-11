4f5a7c7730418c271b01eb75c731e99e4ccfb999
brivanmito <bryanmt496@gmail.com>
2023-07-10 19:04:10 -0500

    Finishing

---
+++ final/Foundation4/Activity.cs
@@ -0,0 +1,30 @@
+public class Activity
+{
+    //setting as DateTime so format can be manipulated
+    private DateTime _date;
+    private double _activityLength;  //in minutes
+
+    public Activity(string date, double activityLength){
+        //capture input as string and parse to DateTime
+        _date = DateTime.Parse(date);
+        _activityLength = activityLength;
+    }
+
+    //Setup default formulas as virtuals
+    public virtual double GetDistance(){
+        return (GetSpeed() * GetActivityLength())/60;
+    }
+    public virtual double GetSpeed(){
+        return (GetDistance() / GetActivityLength())*60;
+    }
+    public virtual double GetPace(){
+        return 60/GetSpeed();
+    }
+    public double GetActivityLength(){
+        return _activityLength;
+    }
+    
+    public string GetSummary(){
+        return $"{_date.ToString("dd MMM yyyy")} {GetType().Name}\t({GetActivityLength()} min) - Distance {GetDistance().ToString("0.00")} miles, Speed {GetSpeed().ToString("0.00")} mph, Pace: {GetPace().ToString("0.00")} min per mile";
+    }
+}
--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        List<Activity> activities = new List<Activity>();
+
+        //building an activity for each type.
+        Running r = new Running(3.4, "06/11/23", 56);
+        Bicyle b = new Bicyle(4.63,"06/11/23",33);
+        Swimming s = new Swimming(10,"07/11/23",23);
+
+        //Adding to the list
+        activities.Add(r);
+        activities.Add(b);
+        activities.Add(s);
+
+        //writing out to console by calling Activity GetSummary method
+        Console.Clear();
+        Console.WriteLine();
+        foreach (Activity a in activities){
+            Console.WriteLine(a.GetSummary());
+        }
+        Console.WriteLine();
     }
 }
